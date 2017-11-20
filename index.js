a57baf5dd03d7680a59f7b3ed28814dc59b79f97
Hope Casey-Allen <hcaseyal@stanford.edu>
2017-11-19 21:13:18 -0800

    Made query summoner name that displays json as text. Worked around the CORS issue by using node.js

--- index.js
+++ index.js
@@ -4,20 +4,23 @@
 
 var analysisArea = d3.select('.analysis-area');
 var visArea = d3.select('.vis-area');
-var baseURL = 'https://na1.api.riotgames.com/lol/';
-var apiKey = 'RGAPI-0c3f4dea-5a44-40ce-b29d-88e00f218389';
 
-function onEnterButtonPress() {
+// Node server url
+var baseURL = "http://localhost:8080/";
+
+function searchSummoner() {
 	var summoner = d3.select('.summoner-name').node().value;
 	visArea.text(summoner);
-	searchForSummoner();
+	searchForSummoner(summoner);
 }
 
-function searchForSummoner() {
+function searchForSummoner(summoner) {
 	var xhttp = new XMLHttpRequest();
-	var url = `${baseURL}summoner/v3/summoners/by-name/RiotSchmick?api_key=${apiKey}`;
+	var url = `${baseURL}summoner/?name=${summoner}`;
 	xhttp.open("GET", url);
-	xhttp.onload = () => analysisArea.text(this.responseText);
+	xhttp.onload = () => {
+		analysisArea.text(xhttp.responseText);
+	}
 	xhttp.onerror = () => analysisArea.text("ERROR " + xhttp.statusText);
 	xhttp.send();
 }
--- server.js
+++ server.js
@@ -2,21 +2,39 @@ var http = require('http');
 const axios = require('axios');
 var express = require('express');
 var app = express();
-const port = 8080; 
 
-app.get('/', function (req, res) {
-	makeRequestToRiot(req, res);
+const port = 8080; 
+var baseURL = 'https://na1.api.riotgames.com/lol/';
+var apiKey = 'RGAPI-0c3f4dea-5a44-40ce-b29d-88e00f218389';
+
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+
+	// Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get('/summoner', function (req, res) {
+	var name = req.param('name');
+	makeRequestToRiot(req, res, name);
 });
 
 app.listen(port, function () {
 	console.log(`Server running at http://localhost:${port}/`)
 });
 
-function makeRequestToRiot(req, res) {
-	var url = 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=RGAPI-0c3f4dea-5a44-40ce-b29d-88e00f218389';
+function makeRequestToRiot(req, res, name) {
+	var url = `${baseURL}summoner/v3/summoners/by-name/${name}?api_key=${apiKey}`;
 	axios.get(url)
 	.then(response => {
 		res.send(response.data);
+		console.log(response);
 	})
 	.catch(error => {
 		res.send(error);
